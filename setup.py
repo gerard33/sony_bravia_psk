278db7d0b9e9296cbf79fd1ebc0aeeae2774e68d
Gerard <gerard33@users.noreply.github.com>
2018-08-18 23:33:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pySonyBraviaPSK',
-    version='0.1.5',
+    version='0.1.6',
     description='Library for Sony Bravia TVs with Pre-Shared Key option',
     url='https://github.com/gerard33/sony_bravia_psk',
     maintainer='Gerard',
-    maintainer_email='aankop3n@gmail.com',
     license='MIT',
     packages=find_packages(),
     install_requires=['requests'],
